e6224d2c2018490265a70e429f3ece0d51da38af
oleg shchepotkin <music.wave.form7@gmail.com>
2023-04-28 16:57:40 +0300

    setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,21 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
+import { FreeMode } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import { useEffect, useState } from 'react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [slideRef, setSlideRef] = useState<any>();
+
   return (
     <>
       <Head>
@@ -14,100 +26,31 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <main>
+      <Swiper
+          spaceBetween= {50}
+          slidesPerView= {5}
+          loop={true}
+          modules={[FreeMode]}
+          freeMode={true}
+          onSwiper={(swiper) => setSlideRef(swiper)}
+      >
+        <SwiperSlide><div className='card'>Slide 1</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 2</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 3</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 4</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 5</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 6</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 7</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 8</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 9</div></SwiperSlide>
+        <SwiperSlide><div className='card'>Slide 10</div></SwiperSlide>
+      </Swiper>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <div className='buttons'>
+        <button onClick={() => slideRef.slidePrev()}>{'<-'}</button>
+        <button onClick={() => slideRef.slideNext()}>{'->'}</button>
+      </div>
       </main>
     </>
   )
